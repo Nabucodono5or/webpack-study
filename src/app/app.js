fac1210df6b6760050ac2d097bac6411cdaebc46
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-11-12 08:07:35 -0300

    fix: code split in different files

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
 const alertService = new AlertService();
-const componentService = new componentService();
+const componentService = new ComponentService();
 const run = (alertService, componentService) => {
   alertService.hideErrors();
 
@@ -7,7 +7,7 @@ const run = (alertService, componentService) => {
     alertService.hideErrors();
 
     const inputs = componentService.getInputs();
-    const parsedInputs = parsedInputs(...inputs);
+    const parsedInputs = parseInputs(...inputs);
     if (inputsAreValid(...parsedInputs)) {
       const [numA, numB] = parsedInputs;
       componentService.setResult(numA + numB);
